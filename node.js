cc1adc0f666c4482b8e5fd502813e8873e961a2a
nin-jin <sairi-na-tenshi@ya.ru>
2025-09-02 13:38:14 +0000

    Deploying to gh-pages from @ hd4ru/space@25a59962a7f48010538aca0834669d64ec398f6a 🚀

--- node.js
+++ node.js
@@ -5768,7 +5768,7 @@ var $;
     (function ($$) {
         class $hd_space_bar_link extends $.$hd_space_bar_link {
             current() {
-                const omit = /^https?:\/\/|\/?\??(#.*)?$/;
+                const omit = /^https?:\/\/|\/?\??(#.*)?$/g;
                 const current = (this.$.$mol_state_arg.value('current') ?? '').replace(omit, '')
                     || this.$.$mol_state_arg.href().replace(omit, '');
                 const uri = this.uri().replace(omit, '');
