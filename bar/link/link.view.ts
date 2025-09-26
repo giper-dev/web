56c4ccbfb8dca664a321bb5c132341f086189d26
jin <jin@hyoo.ru>
2025-09-26 21:48:55 +0300

    build fix

--- bar/link/link.view.ts
+++ bar/link/link.view.ts
@@ -5,5 +5,14 @@ namespace $.$$ {
 			return this.uri().replace( '{query}', encodeURIComponent( this.$.$mol_state_arg.value( 'query' ) ?? '' ) )
 		}
 		
+		@ $mol_mem
+		current() {
+			const omit = /^https?:\/\/|\/?\??(#.*)?$/g
+			const current = ( this.$.$mol_state_arg.value( 'current' ) ?? '' ).replace( omit, '' )
+				|| this.$.$mol_state_arg.href().replace( omit, '' )
+			const uri = this.uri().replace( omit, '' )
+			return uri === current
+		}
+		
 	}
 }
