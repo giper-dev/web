4570223152418ab6abd616dccd8286d2b052db4b
nin-jin <nin-jin@users.noreply.github.com>
2025-10-11 07:10:16 +0000

    Deploying to gh-pages from @ giper-dev/web@be1f7db8562803b02308caa482f8869f08a092ba 🚀

--- node.mjs
+++ node.mjs
@@ -5512,6 +5512,7 @@ var $;
         boxSizing: 'border-box',
         position: 'relative',
         minWidth: rem(2.5),
+        minHeight: rem(2.5),
         gap: $mol_gap.space,
         border: {
             radius: $mol_gap.round,
@@ -5540,11 +5541,6 @@ var $;
                     textShadow: '0 0',
                 }
             }
-        },
-        ':empty': {
-            '::after': {
-                content: '"\u00A0"',
-            },
         },
     });
 })($ || ($ = {}));
